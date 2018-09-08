b7af305a0dad0e51aea74f78c2c958a9d2d77bd1
Irza Pulungan <irzaip@gmail.com>
2018-09-08 09:23:15 +0700

    Change one

--- index.py
+++ index.py
@@ -4,8 +4,9 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello Worldhehe',
         'timestamp': datetime.datetime.utcnow().isoformat()
+        'outme': 'I am the best'
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
