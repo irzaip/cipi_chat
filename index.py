74bec5a70ae63b90bdd0b6713734d3496385eee9
Irza Pulungan <irzaip@gmail.com>
2018-09-08 09:30:52 +0700

    Correct

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import datetime
 def handler(event, context):
     data = {
         'output': 'Hello Worldhehe',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
         'outme': 'I am the best'
     }
     return {'statusCode': 200,
